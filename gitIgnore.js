f67e272c90bfc230f13537914fbe20a6cfd25473
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-19 04:30:43 +0530

    add commands to untrack tracked files

--- gitIgnore.js
+++ gitIgnore.js
@@ -1,2 +1,5 @@
 const ignore = "adding a git ingore file to untrack some files or folder which needed to be secret"
 console.log(ignore);
+const untrack = "to untrack a file which already tracked by git we use the command \n"
+const command = "git rm --cached file name"
+console.log(untrack+command);
