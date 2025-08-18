2489d9737cc54bd1f4db443130cbcbc22943c632
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-19 04:32:10 +0530

    final wrap up

--- gitIgnore.js
+++ gitIgnore.js
@@ -3,3 +3,5 @@ console.log(ignore);
 const untrack = "to untrack a file which already tracked by git we use the command \n"
 const command = "git rm --cached file name"
 console.log(untrack+command);
+const cased = "it will remove the file/folder but keep it locally"
+console.log(cased);
